c60258b5c645475f892176d4445407852834839d
Katherine Wyers <katherine@katherinewyers.com>
2026-06-24 15:13:06 +0100

    Initial commit

---
+++ src/main/java/com/katherinewyers/identity_vault/IdentityVaultApplication.java
@@ -0,0 +1,13 @@
+package com.katherinewyers.identity_vault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/katherinewyers/identity_vault/IdentityVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katherinewyers.identity_vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
